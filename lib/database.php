093dccc8053faa54f3117e905f46d5e44d8530ea
MagmaChocolate <gg845@hotmail.co.jp>
2017-02-17 20:27:52 +0900

    [update]データ保存系の関数全て実装完了
    htmlからajaxで値を受け取るテストはまだやってないけど、それ以降の動作は問題なし。

--- lib/database.php
+++ lib/database.php
@@ -8,6 +8,8 @@
  * ログイン認証を通さないとデータベース内にアクセスし放題なので対策する
 */
 
+
+
 /**
  * 処理ログをテキストファイルとして吐く関数
  * @param  string $text 出力する文字列
@@ -33,16 +35,21 @@ function putLog($text,$type = err){
       $out .= "dir,\n".print_r($text,true)."-----------\n";
       break;
   }
+  chdir(__DIR__);  // ワークディレクトリを戻す
   chdir("../log");  //ワークディレクトリを../logに変更
-  file_put_contents("log.log",$out,FILE_APPEND);
+  if (file_put_contents("log.log",$out,FILE_APPEND) === false){
+    return false;
+  }
+  return true;
 }
 
 /**
- * pageが既存かどうか判断
- * @param  string  $title 記事ID
+ * pageが既存かall_entry_listから判断
+ * @param  string  $title 記事タイトル
  * @return boolean        既存ならtrue、新規はfalse
  */
 function thisPageExisting($title){
+  chdir(__DIR__);  // ワークディレクトリを戻す
   chdir("../db");  //ワークディレクトリを../dbに変更
   $json = file_get_contents("all_entry_list.json");
   $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
@@ -61,16 +68,17 @@ function thisPageExisting($title){
  * 記事IDからその記事の最新バージョンを取得
  * 全記事インデックス(all_entry_list.json)から検索する
  * @param  int    $page 記事ID
- * @return int|fals     最新の記事verを返す、失敗時はfalse
+ * @return int|false     最新の記事verを返す、失敗時はfalse
  */
 function getEntryVer($page){
+  chdir(__DIR__);  // ワークディレクトリを戻す
   chdir("../db");  //ワークディレクトリを../dbに変更
   $json = file_get_contents("all_entry_list.json");
   $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
   $entryList = json_decode($json,true); //連想配列に変換
 
   foreach ($entryList as $value) {
-    if($value['id'] == $id){
+    if($value['id'] == $page){
       return $value['last-ver'];
     }
   }
@@ -84,6 +92,7 @@ function getEntryVer($page){
  * @return int           新規記事IDになる予定の数値を返す
  */
 function willNextEntryId(){
+  chdir(__DIR__);  // ワークディレクトリを戻す
   chdir("../db");  //ワークディレクトリを../dbに変更
   $json = file_get_contents("all_entry_list.json");
   $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
@@ -95,20 +104,26 @@ function willNextEntryId(){
 
 
 /**
- * 記事追加時に記事を管理するindexに追記する
- * @param int    $page   記事ID
- * @param string $title  記事タイトル
- * @param string $author 書き込んだユーザ名
- * @param int    $ip     書き込んだIP
- * @return boolean       成功ならtrue、失敗ならfalse
+ * 記事追加時に記事を管理するindexに追記する、新規ならば新しくファイルを作成する
+ * @param int    $page        記事ID
+ * @param string $title       記事タイトル
+ * @param string $author      書き込んだユーザ名
+ * @param int    $ip          書き込んだIP
+ * @param string $description 編集内容の概要説明
+ * @return boolean            成功ならtrue、失敗ならfalse
  */
-function addEntryIndex($page,$title,$author,$ip){
-  chdir("../db/index");  //ワークディレクトリを../dbに変更
+function addEntryIndex($page,$title,$author,$ip,$description){
   $fileName = $page.".json";
+
+  chdir(__DIR__);  // ワークディレクトリを戻す
+  chdir("../db/entry/index");  //ワークディレクトリを../dbに変更
+  if(!file_exists($fileName)){
+    putLog("addEntryIndex() 書き込み先のファイルが存在しませんでした");
+    return false;
+  }
   $json = file_get_contents($fileName);
   $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
   $entryIndex = json_decode($json,true); //連想配列に変換
-
 
 
   date_default_timezone_set('Asia/Tokyo'); //タイムゾーン設定
@@ -124,23 +139,24 @@ function addEntryIndex($page,$title,$author,$ip){
   $array = array(
     "title" => $title,
     "ver" => getEntryVer($page)+1,
+    "description" => $description,
     "day" => array(
-      "year" => $nowJpTime['year'],
-      "month" => $nowJpTime['month'],
-      "day" => $nowJpTime['day'],
-      "hour" => $nowJpTime['hour'],
-      "minute" => $nowJpTime['minute'],
-      "second" => $nowJpTime['second'],
+      "year" => (int)$nowJpTime['year'],
+      "month" => (int)$nowJpTime['month'],
+      "_day" => (int)$nowJpTime['day'],
+      "hour" => (int)$nowJpTime['hour'],
+      "minute" => (int)$nowJpTime['minute'],
+      "second" => (int)$nowJpTime['second'],
     ),
     "author" => $author,
     "ip" => $ip
   );
 
-  $entryIndex = array_merge($entryIndex,$array);
+  $entryIndex [] = $array;  // 既存の配列と今回作ったデータを結合
 
-  $fp = fopen($fileName,"w");  //上書き
-  fwrite($fp, sprintf(json_encode($entryIndex)));
-  fclose($fp);
+  chdir(__DIR__);  // ワークディレクトリを戻す
+  chdir("../db/entry/index");  //ワークディレクトリを../dbに変更
+  file_put_contents($fileName,sprintf(json_encode($entryIndex,JSON_UNESCAPED_UNICODE)));
 
   return true;
 }
@@ -148,20 +164,15 @@ function addEntryIndex($page,$title,$author,$ip){
 
 /**
  * 記事追加時に記事を管理するindexを新規作成
- * @param string $title  記事タイトル
- * @param string $author 書き込んだユーザ名
- * @param int    $ip     書き込んだIP
+ * @param string $title       記事タイトル
+ * @param string $author      書き込んだユーザ名
+ * @param int    $ip          書き込んだIP
+ * @param string $description 編集の簡易説明
  * @return boolean       成功なら記事ID+1、失敗ならfalse
  */
-function newEmtryIndex($title,$author,$ip){
+function newEntryIndex($page,$title,$author,$ip,$description){
 
-  $page = willNextEntryId();
-
-  chdir("../db/index");  //ワークディレクトリを../dbに変更
   $fileName = $page.".json";
-  $json = file_get_contents($fileName);
-  $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
-  $entryIndex = json_decode($json,true); //連想配列に変換
 
 
 
@@ -178,29 +189,37 @@ function newEmtryIndex($title,$author,$ip){
   $array = array(
     "title" => $title,
     "ver" => 1,
+    "description" => $description,
     "day" => array(
-      "year" => $nowJpTime['year'],
-      "month" => $nowJpTime['month'],
-      "day" => $nowJpTime['day'],
-      "hour" => $nowJpTime['hour'],
-      "minute" => $nowJpTime['minute'],
-      "second" => $nowJpTime['second'],
+      "year" => (int)$nowJpTime['year'],
+      "month" => (int)$nowJpTime['month'],
+      "_day" => (int)$nowJpTime['day'],
+      "hour" => (int)$nowJpTime['hour'],
+      "minute" => (int)$nowJpTime['minute'],
+      "second" => (int)$nowJpTime['second'],
     ),
     "author" => $author,
     "ip" => $ip
   );
 
-  $entryIndex = array_merge($entryIndex,$array);
+  $entryIndex [] = $array;
 
-  touch($fileName);  // ファイル作成
-  chmod($fileName,0777);  // ファイルのパーミッション変更
-  if(!file_exitst($fileName)){
+
+  chdir(__DIR__);  // ワークディレクトリを戻す
+  chdir("../db/entry/index");  //ワークディレクトリを../dbに変更
+  if(file_exists($fileName)){
+    putLog("databace.php newEntryIndex() 作成しようとしたファイル名がすでに存在しているため処理を中断しました。");
+    return false;
+  }
+
+  file_put_contents($fileName,sprintf(json_encode($entryIndex,JSON_UNESCAPED_UNICODE)));
+  chdir(__DIR__);  // ワークディレクトリを戻す
+  chdir("../db/entry/index");  //ワークディレクトリを../dbに変更
+  if(!file_exists ($fileName)){
     putLog("database.php wilEntryIndex() インデックスファイルが作成できませんでした。","err");
     return false;
   }
-  $fp = fopen($fileName,"w");  //上書き
-  fwrite($fp, sprintf(json_encode($entryIndex)));
-  fclose($fp);
+
   return true;
 }
 
@@ -214,7 +233,10 @@ function newEmtryIndex($title,$author,$ip){
  * @return boolean             正常終了はtrue、失敗はfalse
  */
 function saveEntryBody($page,$title,$description,$html){
-  if (thisPageExisting($page)){
+  chdir(__DIR__);  // ワークディレクトリを戻す
+  chdir("../db/entry/body");
+  $tmpFileName = $page."_1.json";
+  if(file_exists($tmpFileName)){
     // 既存記事
     $ver = getEntryVer($page) + 1;
   }else{
@@ -228,11 +250,11 @@ function saveEntryBody($page,$title,$description,$html){
     "description" => $description,
     "html" => $html
   );
-  $json = json_encode($arr,JSON_UNESCAPED_SLASES);  // http://qiita.com/shogo807/items/f68dde0d1fe7c07b8939
-  chdir("../db/body");
-  file_put_contents($fileName, $json);
-  chmod($fileName,0777);  // ファイルのパーミッション変更
-  if(!file_exitst($fileName)){
+  // $json = json_encode($arr,JSON_UNESCAPED_SLASES);  // http://qiita.com/shogo807/items/f68dde0d1fe7c07b8939
+  chdir(__DIR__);  // ワークディレクトリを戻す
+  chdir("../db/entry/body");
+  file_put_contents($fileName,sprintf(json_encode($arr,JSON_UNESCAPED_UNICODE)));
+  if(!file_exists($fileName)){
     putLog("database.php saveEntryBody() 記事bodyが作成できませんでした。","err");
     return false;
   }
@@ -245,8 +267,8 @@ function saveEntryBody($page,$title,$description,$html){
  * @param int    $page     記事id
  * @param string $title    記事タイトル
  * @param string $author   作者名
- * @param array  $category カテゴリを一次配列で渡す
- * @param string $eyecatch アイキャッチ画像へのリンク
+ * @param array  $category [省略可]カテゴリを一次配列で渡す
+ * @param string $eyecatch [省略可]アイキャッチ画像へのリンク
  * @return boolean         正常終了はtrue、失敗はfalse
  */
 function addAllEntryList($page,$title,$author,$category = null,$eyecatch = null){
@@ -267,23 +289,28 @@ function addAllEntryList($page,$title,$author,$category = null,$eyecatch = null)
     "title" => $title,
     "author" => $author,
     "day" => array(
-      "year" => $nowJpTime['year'],
-      "month" => $nowJpTime['month'],
-      "day" => $nowJpTime['day'],
-      "hour" => $nowJpTime['hour'],
-      "minute" => $nowJpTime['minute'],
-      "second" => $nowJpTime['second'],
+      "year" => (int)$nowJpTime['year'],
+      "month" => (int)$nowJpTime['month'],
+      "_day" => (int)$nowJpTime['day'],
+      "hour" => (int)$nowJpTime['hour'],
+      "minute" => (int)$nowJpTime['minute'],
+      "second" => (int)$nowJpTime['second'],
     ),
   );
   if ($eyecatch !== null){  // アイキャッチが指定されているならばindexに記録
     $i = array("eyecatch" => $eyecatch);
     $arr = array_merge($arr,$i);
+  }else{
+    $arr = array_merge($arr,array("eyecatch" => "none"));
   }
   if ($category !== null){  // カテゴリが指定されているならばindexに記録
     $j = array("category" => $category);
     $arr = array_merge($arr,$j);
+  }else{
+    $arr = array_merge($arr,array("category" => "none"));
   }
 
+  chdir(__DIR__);  // ワークディレクトリを戻す
   chdir("../db");  //ワークディレクトリを../dbに変更
   $json = file_get_contents("all_entry_list.json");
   $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
@@ -292,14 +319,17 @@ function addAllEntryList($page,$title,$author,$category = null,$eyecatch = null)
   foreach ($entryList as $key => $value) {
     if($value["id"] == $page){
       $entryKey = $key;
+      break;
     }
   }
-  if (is_set($entryKey)){
+  if (isset($entryKey)){
     $entryList[$entryKey] = $arr; // indexの更新
   }else{
-    $entryList = array_merge($entryList,$arr);  // indexの新規追記
+    $entryList [] = $arr;  // indexの追記
   }
-  file_put_contents("all_entry_list.json",$arr) // 上書きモード
+  chdir(__DIR__);  // ワークディレクトリを戻す
+  chdir("../db/");
+  file_put_contents("all_entry_list.json",sprintf(json_encode($entryList,JSON_UNESCAPED_UNICODE)));
   return true;
 }
 
@@ -318,9 +348,6 @@ function read_db($page,$backlog){
 /**
  * 記事データ本文やインデックスを一括で保存する関数
  * 受け取りはpostでjson形式で送られてくるのでこの関数で受け取る
- * @param  string title
- * @param  int    page  ページ番号、省略すると新規作成モードになる
- * @return [type]       [description]
  */
 function write_db(){
 // function write_db($title,$page = null){
@@ -329,32 +356,54 @@ function write_db(){
     テキストファイルを作り、そのファイル名と作成日時を管理ようテキストファイル
     で管理する
   */
+  $newFlag = !isset($page); //新規記事ならばtrue、既存はfalse
 
+/*
   $json = file_get_contents('php://input');  // http://php-archive.net/php/ajax-json/
-  $data = json_decode($json, true);
+  $data = json_decode($json, true);  // POSTで受け取ったjsonを$dataへ代入
 
-  $page = is_set($data["page"]) ? $data["page"] : willNextEntryId();
+  $page = isset($data["page"]) ? $data["page"] : willNextEntryId();
   $title = $data["title"];
   $html = $data["html"];
   $description = $data["description"];
-  $author = is_set($data["author"]) ? $data["author"] : "名無しさん";
-  $eyecatch = is_set($data["eyecatch"]) ? $data["eyecatch"] : null;
+  $author = isset($data["author"]) ? $data["author"] : "名無しさん";
+  $eyecatch = isset($data["eyecatch"]) ? $data["eyecatch"] : null;
 
   $ip = ip2long($_SERVER["REMOTE_ADDR"]); // アクセス元のipアドレスを取得し整数値に変換して代入
+*/
+// $page = isset($data["page"]) ? $data["page"] : willNextEntryId();
+$page = 1;
+$title = "S＿タイトル";
+$html = "<html><body><h1>ふが</h1></body></html>";
+$description = "S_誤字脱字の修正";
+$author = isset($data["author"]) ? $data["author"] : "名無しさん";
+$eyecatch = isset($data["eyecatch"]) ? $data["eyecatch"] : null;
+$ip = 1234567890;
 
-
-  if($page>=0){  // 既存記事の保存
+$newFlag = false;
+  if($newFlag){
+    // 新規記事の保存
     saveEntryBody($page,$title,$description,$html);
-    addEnteyIndex($page,$title,$author,$ip);
-
-  }else if($page==null){  // 新規記事の保存
+    newEntryIndex($page,$title,$author,$ip,$description);
+    addAllEntryList($page,$title,$author,$category,$eyecatch);
+  }else{
+    // 既存記事の保存
     saveEntryBody($page,$title,$description,$html);
-    addEntryIndex($page,$title,$author,$ip);
+    addEntryIndex($page,$title,$author,$ip,$description);
     addAllEntryList($page,$title,$author,$category,$eyecatch);
   }
 }
 
 ini_set( 'display_errors', 1 ); // エラーログ表示設定
-// chdir("../db");  //ワークディレクトリを../dbに変更
-// addEntryIndex(0,"タイトル","ななしさん","192.168.0.100");
+
+// print thisPageExisting("記事名") ? "true" : "false";
+// print getEntryVer(2);
+// print willNextEntryId();
+// print addEntryIndex(0,"記事タイトル変更したよ","アンノウン",0120117117) ? "success" : "failure";
+// print newEntryIndex("新規記事サンプル","ほげ",0120117117) ? "success" : "failure";
+// print saveEntryBody(5,"新規記事サンプル","新規記事の作成サンプル","<html><body><h1>新規記事サンプル</h1></body></html>") ? "success" :"failure";
+// $i = ["音響","マイク","サンプル"];
+// print addAllEntryList(3,"新規記事サンプル","ほげ",$i,"sample.png") ? "success" : "failure";
+
+write_db();
 ?>
