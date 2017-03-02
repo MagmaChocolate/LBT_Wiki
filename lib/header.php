c00b1150af0b64f3f1b3dfa1da5053bffa5980e7
MagmaChocolate <gg845@hotmail.co.jp>
2017-03-03 01:26:53 +0900

    [fix] metaのtitleが文字化けしていたのを修正

--- lib/header.php
+++ lib/header.php
@@ -9,7 +9,7 @@ $host .= $_SERVER['HTTP_HOST'];
 <!DOCTYPE HTML>
 <html>
 <head>
-  <title><?php echo $fetchData["title"] | LBT_wiki; ?></title>
+  <title><?php echo ($fetchData["title"].' | LBT_wiki'); ?></title>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
