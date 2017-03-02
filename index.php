2ef735358e4bd14607913ded707108f7e15890ac
MagmaChocolate <gg845@hotmail.co.jp>
2017-03-03 01:08:36 +0900

    [update] index.phpにパラメータ追加

--- index.php
+++ index.php
@@ -4,8 +4,46 @@
  * @return [type] [description]
  */
 function init(){
-  if($_GET["cmd"] == "view"){
-    require(__DIR__."/lib/view.php");
+  if(!isset($_GET["cmd"])){
+    require(__DIR__."/lib/home.php");
+    return true;
+  }
+  switch ($_GET["cmd"]){
+    case "view":
+      require(__DIR__."/lib/view.php");
+      break;
+
+    case "edit":
+      if(isset($_GET["page"])){
+        // 既存記事
+        require(__DIR__."/lib/database.php");
+        $info = fetchInfo($_GET["page"]);
+        if($info === false){
+          require("./lib/notfound.php");
+          return true;
+        }
+        require(__DIR__."/lib/editor.php");
+      }else{
+        // 新規記事
+        require(__DIR__."/lib/editor.php");
+      }
+      break;
+
+    case "category":
+      require(__DIR__."/lib/category.php");
+      break;
+
+    case "random":
+      require(__DIR__."/lib/random.php");
+      break;
+
+    case "admin":
+      require(__DIR__."/lib/admin.php");
+      break;
+
+    default:
+      require(__DIR__."/lib/notfound.php");
+      break;
   }
 }
 init();
