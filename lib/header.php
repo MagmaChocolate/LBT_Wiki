f7fd5ba3626be7831685d6fdb7bd0ca7f0ac00fe
MagmaChocolate <magcho@magcho.local>
2017-03-09 16:29:24 +0900

    [add] faviconセットの追加

--- lib/header.php
+++ lib/header.php
@@ -9,6 +9,14 @@ $host .= $_SERVER['HTTP_HOST'];
 <!DOCTYPE HTML>
 <html>
 <head>
+  <!-- favicon -->
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+  <link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+  <link rel="manifest" href="/manifest.json">
+  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000">
+  <meta name="theme-color" content="#ffffff">
+  
   <title><?php echo ($fetchData["title"].' | LBT_wiki'); ?></title>
   <meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
