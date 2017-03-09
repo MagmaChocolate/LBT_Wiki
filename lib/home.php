3bc8a686f7ab30bff89a878371b04bf1cdef8595
MagmaChocolate <gg845@hotmail.co.jp>
2017-03-09 23:59:45 +0900

    [fix]homeの検索窓のプレ入力を修正

--- lib/home.php
+++ lib/home.php
@@ -75,7 +75,7 @@ $host .= $_SERVER['HTTP_HOST'];
             <div class="row">
               <div class="col-xs-2"><i class="fa fa-search fa-lg" ></i></div>
                 <form method="GET" action="search.php">
-                  <input class="search-text col-xs-10" type="text" name="search" value="検索フィード" />
+                  <input class="search-text col-xs-10" type="text" name="search" placeholder="検索フィード" />
                 </form>
             </div>
           </div>
